2227c538a5c58b93d74bcb1c0aa9690f1a6c088e
Болат <sareev9797@mail.ru>
2022-09-19 20:10:52 +0300

    Основa

---
+++ samplesite/bboard/urls.py
@@ -0,0 +1,8 @@
+from .views import index, by_rubric, BbCreateView
+from django.urls import path
+
+urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'),
+    path(('<int:rubric_id>/'), by_rubric, name='by_rubric'),
+    path('', index, name='index'),
+]
